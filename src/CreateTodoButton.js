bd4e694f1886697aaa6e38e8c750465112c889de
ozzkr <OzzKr@users.noreply.github.com>
2023-10-26 11:12:32 -0500

    events added onClick onChange

--- src/CreateTodoButton.js
+++ src/CreateTodoButton.js
@@ -1,8 +1,30 @@
-import './CreateTodoButton.css';
+import "./CreateTodoButton.css";
+
+let count = 0;
+function saludar(message) {
+  console.log("salutaciones", message, count);
+  count++;
+  salutacion(count);
+}
+
+const salutacion = (counter) => {
+  alert("salutacion" + counter);
+};
+
 
 function CreateTodoButton() {
   return (
-    <button className="CreateTodoButton">+</button>
+    <>
+      <button
+        className="CreateTodoButton"
+        onClick={(event) => {
+          saludar("message");
+
+        }}
+      >
+        +
+      </button>
+    </>
   );
 }
 
--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,8 +1,12 @@
-import './TodoSearch.css';
+import React, { useState } from "react";
+import "./TodoSearch.css";
+
+const [state, setState] = React.useState();
 
 function TodoSearch() {
   return (
     <input
+      onChange={(event) => console.log(event.target.value)}
       placeholder="search ToDo Item here"
       className="TodoSearch"
     />
