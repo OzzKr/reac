726ed6ed9bc3bed82a6e7817ab7d5c9963d445e0
ozzkr <OzzKr@users.noreply.github.com>
2023-10-27 16:19:39 -0500

    IconComplete and delete added with svg

---
+++ src/TodoIcon.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { ReactComponent as CheckSvg } from "./check.svg";
+import { ReactComponent as DeleteSvg } from "./delete.svg";
+
+const iconTypes = {
+  check: <CheckSvg />,
+  delete: <DeleteSvg />,
+};
+
+function TodoIcon({ type }) {
+  return (
+    <span className={`Icon Icon-svg Icon-${type}`}>{iconTypes[type]}</span>
+  );
+}
+
+export { TodoIcon };
